af7b21eb9e98da03035dfbbfb08aae0f07f1100e
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-21 22:16:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  counter = 0
-  name_hash.collect do |key, value|
-    
-    counter += 1  
+  smallest_value = 0
+ empty_hash = nil
+ name_hash.each do |name, value|
+  if smallest_value == 0 || value < smallest_value
+    smallest_value = value
+    empty_hash = name
   end
+ end
+ empty_hash
 end
